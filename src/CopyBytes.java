5df4844edef8c5edbffc4b2e21e795697bd776f4
Mihail Hristov <mis6us@icloud.com>
2020-10-03 12:18:50 +0300

    Added file with code. I try for first time to read and write from file in binary format

--- src/CopyBytes.java
+++ src/CopyBytes.java
@@ -1,32 +1,30 @@
+import com.sun.jdi.Value;
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-public class WriteToFile {
+public class CopyBytes {
     public static void main(String[] args) throws IOException {
 
-
         String inputPath = "/Users/mihailhristov/Downloads/04. Java-Advanced-Files-and-Streams-Lab-Resources/input.txt";
-        String outputPath = "output.txt";
+        String output = "output.txt";
 
         FileInputStream in = null;
         FileOutputStream out = null;
 
-        List<Character> punctuation = new ArrayList<>();
-        punctuation.add(',');
-        punctuation.add('.');
-        punctuation.add('!');
-        punctuation.add('?');
-
         try {
             in = new FileInputStream(inputPath);
-            out = new FileOutputStream(outputPath);
+            out = new FileOutputStream(output);
             int oneByte = in.read();
             while (oneByte > -1){
-                if (!punctuation.contains((char)oneByte)){
+                if (oneByte == 32 || oneByte == 10){
                     out.write(oneByte);
+                }else {
+                    String digit = String.valueOf(oneByte);
+                    for (int i = 0; i < digit.length(); i++) {
+                        out.write(digit.charAt(i));
+                    }
                 }
 
                 oneByte = in.read();
