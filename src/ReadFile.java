89ebb752c3f393e6a2b99b8dbd3e824aa26571de
Mihail Hristov <mis6us@icloud.com>
2020-10-03 11:05:03 +0300

    Added file with code. I try for first time to read and write from file in binary format

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -1,2 +1,30 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ReadFile {
+    public static void main(String[] args) throws IOException {
+
+
+        String path = "/Users/mihailhristov/Downloads/04. Java-Advanced-Files-and-Streams-Lab-Resources/input.txt";
+
+        FileInputStream in = null;
+
+        try {
+            in = new FileInputStream(path);
+            int oneByte = in.read();
+            while (oneByte > -1){
+                System.out.print(String.format("%s ", Integer.toBinaryString(oneByte)));
+                oneByte = in.read();
+            }
+        }catch (IOException e){
+            System.out.println("Exception error - missing file");
+        }finally {
+            if (in != null){
+                in.close();
+            }
+        }
+
+
+
+    }
 }
